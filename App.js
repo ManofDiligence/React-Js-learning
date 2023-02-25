a8f4b052bae2c0c1fef62d0159b3197918db4c33
Chi wang So <chiwangso@ChideMacBook-Air.local>
2023-02-25 09:14:03 +0800

    first react web app

---
+++ App.js
@@ -0,0 +1,25 @@
+import ProductList from "./ProductList";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Checkout from "./Checkout";
+import ProductDetail from "./ProductDetail";
+function App() {
+  return (
+    <BrowserRouter>
+      <a href="/checkout">shopping cart1A </a>
+
+      <Link to="/"> Home page</Link>
+      <Link to="/product"> Info about product</Link>
+      <Link to="/checkout"> shopping cartLink</Link>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/product" element={<ProductDetail />}>
+          <Route path=":id" element={<ProductDetail />} />
+        </Route>
+        <Route path="*" element={<p>Can't find the page</p>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
